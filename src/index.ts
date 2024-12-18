d82507dada55278d5e16dc76dcb8b0aee00402a1
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-12-18 10:26:10 +0100

    Update console messages to debug server init

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { connectDB } from "./config/database.js";
 import "reflect-metadata";
 
 import adminRoutes from "./modules/admin/route.js";
+// import authRoutes from "./modules/auth/route.js";
 import userRoutes from "./modules/user/route.js";
 import bookRoutes from "./modules/book/route.js";
 import eventRoutes from "./modules/event/route.js";
 import rsvpRoutes from "./modules/rsvp/route.js";
 import commentRoutes from "./modules/comment/route.js";
-import healthRoutes from "./modules/health/route.js";
 
 dotenv.config();
 
@@ -21,12 +21,12 @@ app.use(express.json());
 
 // Routes
 app.use("/api/admin", adminRoutes);
+// app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/rsvps", rsvpRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/health", healthRoutes);
 
 // Start server and connect to the database
 if (process.env.NODE_ENV !== "test") {
@@ -35,18 +35,20 @@ if (process.env.NODE_ENV !== "test") {
       await connectDB();
 
       const server = app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
+        console.log(`✅ Server running at http://localhost:${PORT}`);
       });
 
       server.on("error", (error: any) => {
         if (error.code === "EADDRINUSE") {
           console.error(
-            `Port ${PORT} is already in use. Choose a different port.`,
+            `❌ Port ${PORT} is already in use. Choose a different port.`,
           );
         } else if (error.code === "EACCES") {
-          console.error(`Permission denied. Unable to bind to port ${PORT}.`);
+          console.error(
+            `❌ Permission denied. Unable to bind to port ${PORT}.`,
+          );
         } else {
-          console.error("Server encountered an error:", error);
+          console.error("❌ Server encountered an error:", error);
         }
         process.exit(1);
       });
