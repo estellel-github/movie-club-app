3737b9e32f0409d38628064d6ad75db8f0525ca9
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-18 11:54:10 +0100

    Add interface: DecodedToken

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,16 @@
 import jwt from "jsonwebtoken";
 
+export interface DecodedToken {
+  user_id: string;
+  role: string;
+}
+
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 
-export const generateToken = (payload: object, expiresIn = "1h") => {
+export const generateToken = (payload: object, expiresIn = "1h"): string => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn });
 };
 
-export const verifyToken = (token: string) => {
-  return jwt.verify(token, JWT_SECRET);
+export const verifyToken = (token: string): DecodedToken => {
+  return jwt.verify(token, JWT_SECRET) as DecodedToken;
 };
