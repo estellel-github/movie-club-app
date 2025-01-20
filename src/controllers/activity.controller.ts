086c601a0abb3f9fd632bf82023b7603aba0bd42
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-20 20:21:45 +0100

    Rename activityLog to activity

---
+++ src/controllers/activity.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express";
+import { ActivityLogService } from "../services/activity.service.js";
+import { CustomError } from "../utils/customError.js";
+
+const activityLogService = new ActivityLogService();
+
+export const createActivityLog = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { type, details, user_id, event_id } = req.body;
+
+    await activityLogService.logActivity(type, details, user_id, event_id);
+
+    res.status(201).json({
+      message: "Activity logged successfully",
+    });
+  } catch (error) {
+    next(
+      error instanceof CustomError
+        ? error
+        : new CustomError("Failed to log activity", 500),
+    );
+  }
+};
