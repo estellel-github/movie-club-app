c5286be2104289eff70d5188bee130e5810c145f
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-20 15:39:25 +0100

    Fix broken import path

--- src/queues/rsvpQueue.ts
+++ src/queues/rsvpQueue.ts
@@ -1,5 +1,5 @@
 import { Queue } from "bullmq";
-import { redisConfig } from "config/redis.js";
+import { redisConfig } from "../config/redis.js";
 
 export const rsvpQueue = new Queue("rsvpQueue", {
   connection: redisConfig,
