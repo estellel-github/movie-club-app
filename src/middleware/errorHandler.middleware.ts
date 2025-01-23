f8bf707fb010201296b37d53b53df1db360228f5
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-23 10:54:03 +0100

    Fix error handler: next is required

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -1,7 +1,12 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { CustomError } from "../utils/customError.js";
 
-export const errorHandler = (err: unknown, req: Request, res: Response) => {
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ error: err.message });
   }
