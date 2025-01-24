a62a597913eba4e7788d883f4075ae7ae7edbcde
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-24 13:40:08 +0100

    Add missing params, fix formatting

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -2,32 +2,32 @@ import { ZodError, type ZodSchema } from "zod";
 import type { Request, Response, NextFunction } from "express";
 import { CustomError } from "../utils/customError.js";
 
-export const validate =
-  (bodySchema?: ZodSchema, querySchema?: ZodSchema) =>
-    (req: Request, res: Response, next: NextFunction) => {
-      try {
-        if (bodySchema) {
-          bodySchema.parse(req.body);
-        }
-
-        if (querySchema) {
-          querySchema.parse(req.query);
-        }
-
-        next();
-      } catch (err: unknown) {
-        if (err instanceof ZodError) {
-          const formattedErrors = err.issues.map((issue) => ({
-            path: issue.path.join("."),
-            message: issue.message,
-            code: issue.code,
-          }));
-
-          return res.status(400).json({
-            errors: formattedErrors,
-          });
-        }
-
-        next(new CustomError("Validation Error", 400));
+export const validate = (bodySchema?: ZodSchema, querySchema?: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (bodySchema) {
+        bodySchema.parse({ ...req.body });
       }
-    };
+
+      if (querySchema) {
+        querySchema.parse({ ...req.query, ...req.params });
+      }
+
+      next();
+    } catch (err: unknown) {
+      if (err instanceof ZodError) {
+        const formattedErrors = err.issues.map((issue) => ({
+          path: issue.path.join("."),
+          message: issue.message,
+          code: issue.code,
+        }));
+
+        return res.status(400).json({
+          errors: formattedErrors,
+        });
+      }
+
+      next(new CustomError("Validation Error", 400));
+    }
+  };
+};
